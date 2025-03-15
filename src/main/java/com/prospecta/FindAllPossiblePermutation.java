59f910682dd470f6251c95b589ed48278b1babc8
updesh <updesh@prospecta.com>
2025-03-15 16:50:15 +0530

    longes subarray sum problem solved

---
+++ src/main/java/com/prospecta/FindAllPossiblePermutation.java
@@ -0,0 +1,2 @@
+package com.prospecta;public class FindAllPossiblePermutation {
+}
--- src/main/java/com/prospecta/LongestSubarrayWithSumK.java
+++ src/main/java/com/prospecta/LongestSubarrayWithSumK.java
@@ -1,20 +1,37 @@
 package com.prospecta;
 
+import java.util.HashMap;
+
 public class LongestSubarrayWithSumK {
 
     public static void main(String[] args) {
 
-        int[] arr = new int[] {-5, 8, -14, 2, 4, 12};
-        int k = -5;
+        int[] arr = new int[] {10, 5, 2, 7, 1, -10};
+        int k = 15;
         int n = arr.length;
-
-        int[] leftToRight = new int[n];
-        int[] rightToLeft = new int[n];
+        HashMap<Long, Integer> prefixSum = new HashMap<>();
+        long sum = 0;
+        int maxLength = 0;
 
         for (int i = 0; i < n; i++) {
+            sum += arr[i];
 
+            if (sum == k) maxLength = Math.max(maxLength, i + 1);
+
+            long checkSum = sum - k;
+            if (prefixSum.get(checkSum) != null) {
+                maxLength = Math.max(maxLength, i - prefixSum.get(checkSum));
+            }
+
+            if (!prefixSum.containsKey(sum)) {
+                prefixSum.put(sum, i);
+            }
         }
 
+        System.out.println(maxLength);
+
     }
+
+
 
 }
